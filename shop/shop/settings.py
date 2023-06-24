495b07c2a2f7f2e17a52a8f4cda0bc9b4e708f3c
shakirri <ri8shakir@gmail.com>
2023-06-24 17:29:42 +0600

    Version 2.1
    postgresql inclusion

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
